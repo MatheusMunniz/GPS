8478f5adeacde8d52a369be400042c8ccdc9aacb
MatheusMunniz <matheus.muniz.085@ufrn.edu.br>
2023-06-15 10:36:58 -0300

    Educational player

--- js/thirdpage.js
+++ js/thirdpage.js
@@ -25,7 +25,7 @@ function backToTop() {
 }
 
 
-// SEARCH BY GENE //
+// SEARCH BY GENE
 
 //Location bar
 
@@ -105,3 +105,16 @@ function mostrarTodasLinhas() {
     linhas[i].style.display = "";
   }
 }
+
+
+// Educational
+
+$('#play').on('click', function (e) {
+  e.preventDefault();
+  $("#player")[0].src += "?autoplay=1";
+  $('#player').show();
+  $('#video-cover').hide();
+})
+$('#modal1').on('hidden.bs.modal', function (e) {
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
