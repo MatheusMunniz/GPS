0402cce2a9875ebfe4d85e36ce3da6bde4f042db
MatheusMunniz <matheus.muniz.085@ufrn.edu.br>
2023-08-16 14:44:48 -0300

    profile box finished

--- js/secondpage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.custom-control-input').change(function() {
-      if ($(this).is(':checked')) {
-        $(this).closest('.list-group-item').find('.expandable-section').slideDown();
-      } else {
-        $(this).closest('.list-group-item').find('.expandable-section').slideUp();
-      }
-    });
-  });
